93a41781e3c261364fa9cf9b4941d19d312d787c
Oyindamola10 <samiatadegbenro@gmail.com>
2023-10-12 08:10:29 +0100

    i rendered five(5) components into each other

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,16 @@
+import Greeting from "./components/Greeting"
+const App =() => {
+  
+
+  return (
+    <div>
+      <h1>Awesome React.js</h1>
+      <Greeting/>
+      <Greeting/>
+      <Greeting/>
+    </div>
+  
+  )
+}
+
+export default App;
---
+++ hello-react/src/components/Introduction.jsx
@@ -0,0 +1,13 @@
+const Introduction = () => {
+    return (
+        <div>
+            <h3>About Me</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Repellat itaque reiciendis suscipit, recusandae eius
+                  nostrum debitis sunt quo fugiat beatae saepe tenetur inventore,
+                 laudantium magnam nesciunt ex ad minus necessitatibus!</p>
+        </div>
+    )
+}
+
+export default Introduction;
